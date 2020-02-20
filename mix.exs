8914c2f340397af8117d4efbec40e61eadbd556a
Fabian Zitter <fabian.zitter@gmail.com>
2020-02-20 12:14:16 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NiceMaps.MixProject do
   def project do
     [
       app: :nice_maps,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
